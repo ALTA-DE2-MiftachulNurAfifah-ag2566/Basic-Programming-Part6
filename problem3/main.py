84dd8dd284da6906e12c2132945a6984c75b3270
miftachulnafifa <miftachulnurafifahnurdin@yahoo.com>
2024-03-07 18:40:40 +0700

    solve problem

--- problem3/main.py
+++ problem3/main.py
@@ -1,6 +1,11 @@
 def array_unique(arrayA, arrayB):
-    # your code here
-    return ''
+    # jum_digit = {}
+    digit_muncul_sekali = []
+    for digit in arrayA:
+        if digit not in arrayB:
+            digit_muncul_sekali.append(digit)
+    
+    return digit_muncul_sekali
 
 if __name__ == '__main__':
     print(array_unique([1, 2, 3, 4], [1, 3, 5, 10, 16])) # [2, 4]
