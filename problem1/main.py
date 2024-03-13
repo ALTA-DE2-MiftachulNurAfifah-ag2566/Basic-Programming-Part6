8d05b5aed1f4f27f7937fa7412f91ffdc45f71c6
miftachulnafifa <miftachulnurafifahnurdin@yahoo.com>
2024-03-13 17:16:52 +0700

    solve problem

--- problem1/main.py
+++ problem1/main.py
@@ -1,5 +1,20 @@
 def compare(a, b):
     pattern = ""
+    a = list(a)
+    b = list(b)
+    # print(a, b)
+    if len(a) < len(b):
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                continue
+            else:
+                pattern += a[i]
+    else:
+        for i in range(len(b)):
+            if a[i] != b[i]:
+                continue
+            else:
+                pattern += a[i]
     return pattern
 
 if __name__ == '__main__':
