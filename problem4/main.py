6a673858150909e58a1ad83c37d77185dbe85046
miftachulnafifa <miftachulnurafifahnurdin@yahoo.com>
2024-03-13 17:26:14 +0700

    solve problem

--- problem4/main.py
+++ problem4/main.py
@@ -1,5 +1,11 @@
 def find_max_sum_sub_array(k, arr):
-    return 0
+    max_sum = sum(arr[:k]) 
+    sum_now = max_sum
+    for i in range(k, len(arr)): 
+        sum_now = sum_now + arr[i] - arr[i-k]
+        max_sum = max(max_sum, sum_now)
+    return max_sum
+    # return 0
 
 if __name__ == '__main__':
     print(find_max_sum_sub_array(3, [2, 1, 5, 1, 3, 2])) # 9
